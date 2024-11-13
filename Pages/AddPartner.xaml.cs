c9c1ba65d1488d6f4f17e2e7f4e632376788f7ef
FedorProgrammer123 <fedorlohkin@gmail.com>
2024-11-13 14:47:01 +0300

    AddXamlAddPartners

--- Pages/AddPartner.xaml.cs
+++ Pages/AddPartner.xaml.cs
@@ -30,5 +30,57 @@ namespace KT7_SkalochkinFedor_ISP_321_MasterFloor.Pages
         {
             Classes.Manager.MainFrame.Navigate(new Pages.ModuleForAccountingPartners());
         }
+
+        private void AddPartnerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (EnterTitle.Text == "" || EnterTitle.Text == " " || EnterTitle.Text == null)
+            {
+                string message = "Ошибка! Наименование не заполнено";
+                
+                MessageBoxImage image = MessageBoxImage.Error;
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxResult result = MessageBox.Show("наименование не введено",message,button,image);
+            }
+            else if (TypePartner.SelectedItem == null)
+            {
+                string message = "Ошибка! Тип не выбран";
+
+                MessageBoxImage image = MessageBoxImage.Error;
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxResult result = MessageBox.Show("Тип не выбран", message, button, image);
+            }
+            else if (EnterRating.Text == "" || EnterRating.Text == " " || EnterRating.Text == null)
+            {
+                string message = "Ошибка! Рейтинг не указан";
+
+                MessageBoxImage image = MessageBoxImage.Error;
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxResult result = MessageBox.Show("Рейтинг не указан", message, button, image);
+            }
+            else if (EnterAddress.Text == "" || EnterAddress.Text == " " || EnterAddress.Text == null)
+            {
+                string message = "Ошибка! Адресс не указан";
+
+                MessageBoxImage image = MessageBoxImage.Error;
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxResult result = MessageBox.Show("Адресс не указан", message, button, image);
+            }
+            else if (EnterFIODirector.Text == "" || EnterFIODirector.Text == " " || EnterFIODirector.Text == null)
+            {
+                string message = "Ошибка! ФИО не указан";
+
+                MessageBoxImage image = MessageBoxImage.Error;
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxResult result = MessageBox.Show("ФИО не указан", message, button, image);
+            }
+            else if (EnterPhone.Text == "" || EnterPhone.Text == " " || EnterPhone.Text == null)
+            {
+                string message = "Ошибка! Телефон не указан";
+
+                MessageBoxImage image = MessageBoxImage.Error;
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxResult result = MessageBox.Show("Телефон не указан", message, button, image);
+            }
+        }
     }
 }
