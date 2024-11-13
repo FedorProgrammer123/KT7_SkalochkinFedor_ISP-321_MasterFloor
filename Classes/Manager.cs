45dea1b899de2bfa62bd56800992ee7991560de3
FedorProgrammer123 <fedorlohkin@gmail.com>
2024-11-13 13:13:27 +0300

    AddStructureAndManager

---
+++ Classes/Manager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT7_SkalochkinFedor_ISP_321_MasterFloor.Classes
+{
+    class Manager
+    {
+    }
+}
